c9e5ff2f7bfc8d15a72271cf070325c9afb4c02a
Nguyen Dang Minh <nguyendangminh@gmail.com>
2017-06-03 12:00:47 +0700

    Động tthổ

---
+++ request.go
@@ -0,0 +1,57 @@
+package httputil
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+	"github.com/pkg/errors"
+)
+
+const REQUEST_TIMEOUT int = 3 // seconds
+
+func Get(endpoint string) ([]byte, error) {
+	return request("GET", endpoint, nil)
+}
+
+func Post(endpoint string, data []byte) ([]byte, error) {
+	return request("POST", endpoint, data)
+}
+
+func Put(endpoint string, data []byte) ([]byte, error) {
+	return request("PUT", endpoint, data)
+}
+
+func Delete(endpoint string) ([]byte, error) {
+	return request("DELETE", endpoint, nil)
+}
+
+func request(method string, endpoint string, data []byte) ([]byte, error) {
+	req, err := http.NewRequest(method, endpoint, bytes.NewReader(data))
+	if err != nil {
+		return nil, errors.Wrapf(err, "NewRequest failed: %s %s\n", method, endpoint)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{
+		Timeout: time.Duration(REQUEST_TIMEOUT) * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrapf(err, "HTTP Do failed")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrapf(err, "ReadAll failed: body=%+v\n", resp.Body)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+    	return nil, fmt.Errorf("%s\n%s", resp.StatusCode, string(body))
+    }
+
+	return body, nil
+}
